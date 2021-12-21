bffbb7e1892be1be8b4c79c48ff13b76654a52cb
William Zembricki <wzembricki@gmail.com>
2021-12-21 14:26:13 -0500

    failing to compile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { APP_URL } from ".";
 function App() {
+  // This is the fetch for the log in. need to develop app url and also find where this will be stored
+  fetch(`${APP_URL}/api/v1/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      user: {
+        username: this.state.username,
+        password: this.state.password,
+      },
+    }),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      if (result.authenticated) {
+        localStorage.setItem("jwt_token", result.token);
+        this.props.updateCurrentUser(result.user.data);
+      } else {
+        alert("Password/Username combination not found");
+      }
+    });
   return (
     <div className="App">
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
+// Creating action cable for the app to use
+const CableApp = {};
+CableApp.cable = actionCable.createConsumer(APP_CABLE_URL);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App cableApp={CableApp} />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+export const APP_URL = "http:localhost:3000";
+export const APP_CABLE_URL = "ws://localhost:3000/cable";
